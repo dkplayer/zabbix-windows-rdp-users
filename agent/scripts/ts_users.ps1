e7de6d5fb28fe6724ea3d399bcbb181b61cb4c1d
Konstantin Dyachenko <dkplayer@gmail.com>
2021-12-21 15:15:04 +0500

    start

---
+++ agent/scripts/ts_users.ps1
@@ -0,0 +1,18 @@
+﻿ function Get-TSSessions {
+    param(
+        $ComputerName = "localhost"
+    )
+
+    qwinsta /server:$ComputerName |
+    #Parse output
+    ForEach-Object {
+        $_.Trim() -replace "\s+",","
+    } |
+    #Convert to objects
+    ConvertFrom-Csv
+}
+
+
+#Get-TSSessions -ComputerName "localhost"
+Get-TSSessions -ComputerName "localhost" | where -Property СЕАНС -Like "rdp-tcp#*" | select ПОЛЬЗОВАТЕЛЬ -ExpandProperty ПОЛЬЗОВАТЕЛЬ
+Get-TSSessions -ComputerName "localhost" | where -Property SESSIONNAME -Like "rdp-tcp#*" | select USERNAME -ExpandProperty USERNAME 
